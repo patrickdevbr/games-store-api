c5aaefa0bac39e2b9899fa9c78f4a5d394530273
Patrick-Ribeiro <patrickribeiro.rb@gmail.com>
2021-06-21 10:13:07 -0300

    Dev profile

--- src/main/java/br/com/supera/game/store/GamesStoreApplication.java
+++ src/main/java/br/com/supera/game/store/GamesStoreApplication.java
@@ -2,8 +2,6 @@ package br.com.supera.game.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class GamesStoreApplication {
